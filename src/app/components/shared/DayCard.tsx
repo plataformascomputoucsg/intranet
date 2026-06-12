971b00ed715d8e6506eb974bc7a279dbc6ca67b3
plataformascomputoucsg <plataformas.computo@cu.ucsg.edu.ec>
2026-06-12 16:44:55 -0500

    fix: cambio

--- src/app/components/shared/DayCard.tsx
+++ src/app/components/shared/DayCard.tsx
@@ -7,19 +7,35 @@ interface DayCardProps {
   onClick?: () => void;
 }
 
-const DayCard: React.FC<DayCardProps> = ({ dayNumber, dayName, isActive = false, onClick }) => {
+const DayCard: React.FC<DayCardProps> = ({
+  dayNumber,
+  dayName,
+  isActive = false,
+  onClick,
+}) => {
   return (
     <div
       className={`relative h-16 w-14 cursor-pointer rounded-lg outline -outline-offset-1 transition-all ${
-        isActive ? 'bg-rose-800/10 outline-1 outline-rose-800' : 'bg-white outline-zinc-300'
+        isActive
+          ? 'bg-rose-800 outline-rose-800'
+          : 'bg-white outline-zinc-300'
       }`}
       onClick={onClick}
     >
       <div className="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center gap-2.5 self-center">
-        <div className="justify-start self-stretch text-center font-['Poppins'] text-sm leading-4 font-medium text-stone-500">
+        <div
+          className={`self-stretch text-center font-['Poppins'] text-sm leading-4 font-medium ${
+            isActive ? 'text-white' : 'text-stone-500'
+          }`}
+        >
           {dayName}
         </div>
-        <div className="justify-start self-stretch text-center font-['Poppins'] text-2xl leading-4 font-medium text-black">
+
+        <div
+          className={`self-stretch text-center font-['Poppins'] text-2xl leading-4 font-medium ${
+            isActive ? 'text-white' : 'text-black'
+          }`}
+        >
           {dayNumber}
         </div>
       </div>
--- src/app/components/shared/MonthCard.tsx
+++ src/app/components/shared/MonthCard.tsx
@@ -6,7 +6,11 @@ interface MonthCardProps {
   onClick?: () => void;
 }
 
-const MonthCard: React.FC<MonthCardProps> = ({ month, isActive = false, onClick }) => {
+const MonthCard: React.FC<MonthCardProps> = ({
+  month,
+  isActive = false,
+  onClick,
+}) => {
   return (
     <div
       className="relative flex w-full cursor-pointer flex-col items-center gap-4"
@@ -15,22 +19,33 @@ const MonthCard: React.FC<MonthCardProps> = ({ month, isActive = false, onClick 
     >
       <div
         className={`relative h-16 w-full rounded-lg outline -outline-offset-1 ${
-          isActive ? 'bg-rose-800/5 outline-rose-800' : 'bg-white outline-zinc-300'
+          isActive
+            ? 'bg-rose-800 outline-rose-800'
+            : 'bg-white outline-zinc-300'
         }`}
       >
         <div className="relative flex h-full w-full flex-col items-center justify-center gap-2.5 px-2">
           <div
             className={`text-center font-['Poppins'] text-base leading-3 font-medium whitespace-nowrap ${
-              isActive ? 'text-rose-800' : 'text-black'
+              isActive ? 'text-white' : 'text-black'
             }`}
           >
             {month}
           </div>
         </div>
       </div>
-      <div className="relative top-0 left-0 inline-flex items-center justify-center gap-[3px]">
-        <div className="h-[5px] w-[5px] rounded-[38px] bg-teal-400" />
-        <div className="h-[5px] w-[5px] rounded-[38px] bg-teal-400" />
+
+      <div className="relative inline-flex items-center justify-center gap-[3px]">
+        <div
+          className={`h-[5px] w-[5px] rounded-full ${
+            isActive ? 'bg-white' : 'bg-teal-400'
+          }`}
+        />
+        <div
+          className={`h-[5px] w-[5px] rounded-full ${
+            isActive ? 'bg-white' : 'bg-teal-400'
+          }`}
+        />
       </div>
     </div>
   );
