d6e29057e6c5c98ddd1f8e25b20b9b0c0b513a70
plataformascomputoucsg <plataformas.computo@cu.ucsg.edu.ec>
2026-06-30 16:20:03 -0500

    Revert "fix: cambio"
    This reverts commit f6c1e3de734f05ceaf1705307ec8bb15bae1590c.

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -7,12 +7,12 @@ const Header: React.FC = () => {
     // <header className="bg-white py-6 px-4 md:px-12 flex flex-col md:flex-row justify-between items-center shadow-sm">
 
     <header className="h-40 w-full bg-white">
-      <div className="mx-auto grid w-[95%] max-w-[1920px] grid-cols-[1fr_auto_1fr] py-10 xl:w-[80%]">
+      <div className="mx-auto grid w-[95%] max-w-[1920px] grid-cols-[1fr_auto_1fr] py-8 xl:w-[80%]">
         <div className="flex justify-start">
-          <UcsgIcon className="h-12 w-auto lg:h-12" />
+          <UcsgIcon className="h-12 w-auto lg:h-14" />
         </div>
         <div className="flex justify-center">
-          <IntranetIcon className="h-10 w-auto lg:h-10" />
+          <IntranetIcon className="h-10 w-auto lg:h-12" />
         </div>
         <div />
       </div>
